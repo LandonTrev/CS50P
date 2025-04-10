0c8bd3391ba3cfbe733c17208ea30858d9c1787b
Landon Trevisani <landon.trevisani@gmail.com>
2025-04-10 19:17:42 -0400

    return values short video

---
+++ Fundamentals/greetings(variables).py
@@ -0,0 +1,8 @@
+def greet(input):
+    if "Hello" in input:
+        return "Hello, there!"
+    else: 
+        return "Im not sure what else you mean !"
+
+greeting = greet("hello, computer")
+print(greeting +" Landon")

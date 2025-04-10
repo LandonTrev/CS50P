b5af27fe4965d5dd12c379afd75b97150dfac322
Landon Trevisani <landon.trevisani@gmail.com>
2025-04-10 19:45:45 -0400

    Side Effects and String Methods

--- Fundamentals/greetings(variables).py
+++ Fundamentals/greetings(variables).py
@@ -1,8 +1,9 @@
 def greet(input):
-    if "Hello" in input:
-        return "Hello, there!"
+    if "hello" in input.lower():
+        return "hello, there"
     else: 
-        return "Im not sure what else you mean !"
+        return "Im not sure what else you mean!"
+
 
 greeting = greet("hello, computer")
 print(greeting +" Landon")

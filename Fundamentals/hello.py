6f7856d8d8c23d723024bc739c6b362d074d7bf3
Landon Trevisani <landon.trevisani@gmail.com>
2025-04-10 11:26:29 -0400

    Upload Python fundamentals from CS50 course

--- Fundamentals/hello.py
+++ Fundamentals/hello.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# Ask User for their name
+name = input("Whats your name?").strip().title()
+
+#Split users name into first and last name
+first, last = name.split(" ")
+
+# Say hello to user
+print(f"Hello, {first}") 
